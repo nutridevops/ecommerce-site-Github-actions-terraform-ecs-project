87bec792c78c1ceed4cc2b953249f857fde81780
nutridevops <nutridevops@gmail.com>
2023-05-16 05:25:56 +0000

    updated files

--- iac/data-source.tf
+++ iac/data-source.tf
@@ -1,4 +1,4 @@
-# retrieve the secret value stored in secrets manager and parse it as a json object
+# retrieve the secret value stored in Secrets Manager and parse it as a json object
 data "aws_secretsmanager_secret_version" "secrets" {
   secret_id = var.secrets_manager_secret_name
 }
